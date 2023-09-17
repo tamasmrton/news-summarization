ea37462ada0f439c58bd3e248aac024b796b9aa0
Tamas Marton <martontam@gmail.com>
2023-09-17 10:01:41 +0200

    fix pep8 issue

--- news_summarization/news_extractor.py
+++ news_summarization/news_extractor.py
@@ -13,7 +13,8 @@ import requests
 
 log = logging.getLogger(__name__)
 
-class NewsExtractor:    # pylint: disable=too-many-instance-attributes
+
+class NewsExtractor:  # pylint: disable=too-many-instance-attributes
     """
     A utility class for extracting news items from website sitemaps based on a given date.
 
@@ -43,8 +44,10 @@ class NewsExtractor:    # pylint: disable=too-many-instance-attributes
         :param date_format: The format of the date string.
         """
         self.base_url = base_url
-        self.headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-                         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+                         AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+        }
         self.date = date
         self.date_format = date_format
         self.date_formatted = datetime.strptime(date, date_format)
@@ -58,24 +61,26 @@ class NewsExtractor:    # pylint: disable=too-many-instance-attributes
 
         :return: A list of sitemap URLs.
         """
-        robots = f'{self.base_url}/robots.txt'
+        robots = f"{self.base_url}/robots.txt"
         try:
-            log.info('Fetching %s...', robots)
+            log.info("Fetching %s...", robots)
             response = requests.get(robots, headers=self.headers, timeout=60)
         except requests.exceptions.RequestException as err:
-            log.exception('Unable to fetch url; err=%s', err)
+            log.exception("Unable to fetch url; err=%s", err)
 
-        log.info('Retrieving sitemaps...')
+        log.info("Retrieving sitemaps...")
         data = []
         lines = str(response.text).splitlines()
         for line in lines:
             if line.rstrip():
-                if not line.startswith('#'):
-                    split = line.split(':', maxsplit=1)
+                if not line.startswith("#"):
+                    split = line.split(":", maxsplit=1)
                     data.append([split[0].strip(), split[1].strip()])
-        sitemaps = [directive[1] for directive in data if directive[0].lower() == 'sitemap']
+        sitemaps = [
+            directive[1] for directive in data if directive[0].lower() == "sitemap"
+        ]
         if not sitemaps:
-            log.exception('Sitemaps were not found.')
+            log.exception("Sitemaps were not found.")
         return sitemaps
 
     def _parse_sitemap_with_lastmod(self, soup):
@@ -86,18 +91,18 @@ class NewsExtractor:    # pylint: disable=too-many-instance-attributes
         :return: A list of links from the sitemap that match the last modification date criteria.
         """
         links = []
-        lastmod_tags = soup.find_all('lastmod')
+        lastmod_tags = soup.find_all("lastmod")
         for lastmod_tag in lastmod_tags:
             lastmod_date_str = lastmod_tag.text
             lastmod_date = datetime.strptime(lastmod_date_str, "%Y-%m-%dT%H:%M:%S%z")
 
             date_formatted_tz = self.date_formatted.replace(tzinfo=lastmod_date.tzinfo)
             if lastmod_date >= date_formatted_tz:
-                loc_tag = lastmod_tag.find_previous('loc')
+                loc_tag = lastmod_tag.find_previous("loc")
                 if loc_tag:
                     link = loc_tag.text
-                    date_pattern = rf'{self.year}.*?{self.month}.*?{self.day}'
-                    if link.endswith('.xml'):
+                    date_pattern = rf"{self.year}.*?{self.month}.*?{self.day}"
+                    if link.endswith(".xml"):
                         links += self.parse_sitemap(link)
                     elif re.search(date_pattern, link):
                         links.append(link)
@@ -111,12 +116,12 @@ class NewsExtractor:    # pylint: disable=too-many-instance-attributes
         :return: A list of links from the sitemap that match the target date criteria.
         """
         links = []
-        loc_tags = soup.find_all('loc')
+        loc_tags = soup.find_all("loc")
         for loc_tag in loc_tags:
             link = loc_tag.text
-            date_pattern = rf'{self.year}.*?{self.month}.*?{self.day}'
+            date_pattern = rf"{self.year}.*?{self.month}.*?{self.day}"
             if re.search(date_pattern, link):
-                if link.endswith('.xml'):
+                if link.endswith(".xml"):
                     links += self.parse_sitemap(link)
                 else:
                     links.append(link)
@@ -129,20 +134,20 @@ class NewsExtractor:    # pylint: disable=too-many-instance-attributes
         :param sitemap: The URL of the sitemap to parse.
         :return: A list of links from the sitemap that match the date criteria.
         """
-        log.info('Parsing sitemap: %s...', sitemap)
+        log.info("Parsing sitemap: %s...", sitemap)
         response = requests.get(sitemap, headers=self.headers, timeout=60)
-        soup = BeautifulSoup(response.content, 'xml')
+        soup = BeautifulSoup(response.content, "xml")
         links = []
 
-        if soup.find('lastmod'):
-            log.info('Last modified date found, parsing...')
+        if soup.find("lastmod"):
+            log.info("Last modified date found, parsing...")
             links = self._parse_sitemap_with_lastmod(soup)
         else:
-            log.info('Last modified date not found, parsing via date...')
+            log.info("Last modified date not found, parsing via date...")
             links = self._parse_sitemap_without_lastmod(soup)
 
         unique_links = list(set(links))
-        log.info('Found links: %s', len(unique_links))
+        log.info("Found links: %s", len(unique_links))
         return unique_links
 
     def extract_news(self, link: str) -> str:
@@ -152,15 +157,14 @@ class NewsExtractor:    # pylint: disable=too-many-instance-attributes
         :param link: The URL of the news article.
         :return: Extracted news content as a string.
         """
-        log.info('Fetching news from %s', link)
+        log.info("Fetching news from %s", link)
         response = trafilatura.fetch_url(link)
         if response:
             news = trafilatura.extract(response, favor_precision=True)
-            log.info('News fetched!')
-            return news
+            log.info("News fetched!")
         if not response:
-            log.exception(
-                'Failed to retrieve text from URL: "%s"', link)
+            log.exception('Failed to retrieve text from URL: "%s"', link)
+        return news
 
     def main(self, delay: int = 10) -> tuple:
         """
@@ -179,5 +183,5 @@ class NewsExtractor:    # pylint: disable=too-many-instance-attributes
                     news_items.append(news)
                     sleep(delay)
             else:
-                log.exception('No news items to fetch!')
+                log.exception("No news items to fetch!")
         return links, news_items
