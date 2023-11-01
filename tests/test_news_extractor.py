cb1bb05cfe1709dc94898d5eb8d7f67929f9a2d0
Tamas Marton <martontam@gmail.com>
2023-11-01 22:13:20 +0100

    fix pytest

--- tests/test_news_extractor.py
+++ tests/test_news_extractor.py
@@ -28,21 +28,26 @@ MOCK_SITEMAP_CONTENT = """
     </urlset>
 """
 
+
 @pytest.fixture
 def news_extractor():
     """
     Create a NewsExtractor instance for testing.
     """
-    return NewsExtractor(base_url='https://example.com', date='2023-08-13', date_format='%Y-%m-%d')
+    return NewsExtractor(
+        base_url="https://example.com", date="2023-08-13", date_format="%Y-%m-%d"
+    )
+
 
 def test_init(news_extractor):  # pylint: disable=redefined-outer-name
     """
     Test the initialization of the NewsExtractor instance.
     """
-    assert news_extractor.base_url == 'https://example.com'
-    assert news_extractor.date == '2023-08-13'
-    assert news_extractor.date_format == '%Y-%m-%d'
-    assert news_extractor.date_formatted == datetime.strptime('2023-08-13', '%Y-%m-%d')
+    assert news_extractor.base_url == "https://example.com"
+    assert news_extractor.date == "2023-08-13"
+    assert news_extractor.date_format == "%Y-%m-%d"
+    assert news_extractor.date_formatted == datetime.strptime("2023-08-13", "%Y-%m-%d")
+
 
 def test_get_sitemaps(news_extractor, mocker):  # pylint: disable=redefined-outer-name
     """
@@ -51,30 +56,43 @@ def test_get_sitemaps(news_extractor, mocker):  # pylint: disable=redefined-oute
     # Mock the requests.get method and its response
     response_mock = Mock()
     response_mock.text = "sitemap: https://example.com/sitemap.xml"
-    mocker.patch('requests.get', return_value=response_mock)
+    mocker.patch("requests.get", return_value=response_mock)
 
     sitemaps = news_extractor.get_sitemaps()
 
     assert len(sitemaps) == 1
-    assert sitemaps[0] == 'https://example.com/sitemap.xml'
+    assert sitemaps[0] == "https://example.com/sitemap.xml"
 
-def test_parse_sitemap_with_lastmod(news_extractor):    # pylint: disable=redefined-outer-name
+
+def test_parse_sitemap_with_lastmod(
+    news_extractor,
+):  # pylint: disable=redefined-outer-name
     """
     Test parsing a sitemap with lastmod tags.
     """
-    soup = BeautifulSoup(MOCK_SITEMAP_CONTENT, 'xml')
-    links = news_extractor._parse_sitemap_with_lastmod(soup)    # pylint: disable=protected-access
+    soup = BeautifulSoup(MOCK_SITEMAP_CONTENT, "xml")
+    links = []
+    links = news_extractor._parse_sitemap_with_lastmod(
+        soup, links
+    )  # pylint: disable=protected-access
 
     assert len(links) == 1
 
-def test_parse_sitemap_without_lastmod(news_extractor): # pylint: disable=redefined-outer-name
+
+def test_parse_sitemap_without_lastmod(
+    news_extractor,
+):  # pylint: disable=redefined-outer-name
     """
     Test parsing a sitemap without lastmod tags.
     """
-    soup = BeautifulSoup(MOCK_SITEMAP_CONTENT, 'xml')
-    links = news_extractor._parse_sitemap_without_lastmod(soup) # pylint: disable=protected-access
+    soup = BeautifulSoup(MOCK_SITEMAP_CONTENT, "xml")
+    links = []
+    links = news_extractor._parse_sitemap_without_lastmod(
+        soup, links
+    )  # pylint: disable=protected-access
 
     assert len(links) == 2
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pytest.main()
